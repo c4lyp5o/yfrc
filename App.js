fffc12700bc548d24c9f3ef9cc010e07fe8d5302
Mohammad Aimaan <calypso@calypsocloud.one>
2024-01-17 22:29:15 +0800

    experimental

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
+import React, { useState } from 'react';
+import { View, useWindowDimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TabView, SceneMap, TabBarItem } from 'react-native-tab-view';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
 import { config } from '@gluestack-ui/config';
 import {
   Box,
@@ -13,90 +24,17 @@ import Gradient from './assets/Icons/Gradient';
 import DocumentData from './assets/Icons/DocumentData';
 import LightBulbPerson from './assets/Icons/LightbulbPerson';
 import Rocket from './assets/Icons/Rocket';
-import { useState } from 'react';
 
-export default function App() {
-  return (
-    <GluestackUIProvider config={config}>
-      <Home />
-    </GluestackUIProvider>
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Screen } from 'react-native-screens';
+
+function Demo() {
+  const layout = useWindowDimensions();
+  const insets = useSafeAreaInsets();
+
+  const [url, setUrl] = useState(
+    'https://api.waktusolat.me/waktusolat/today/kdh01'
   );
-}
-
-const Home = () => {
-  return <Container />;
-};
-
-const FeatureCard = ({ iconSvg: IconSvg, name, statusCode }) => {
-  return (
-    <Box
-      flexDirection='column'
-      borderWidth={1}
-      borderColor='$borderDark700'
-      sx={{
-        _web: {
-          flex: 1,
-        },
-      }}
-      m='$2'
-      p='$4'
-      rounded='$md'
-    >
-      <Box alignItems='center' display='flex' flexDirection='row'>
-        {/* <Image source={iconSvg} alt="document" width={22} height={22} /> */}
-        <Text>
-          <IconSvg />
-        </Text>
-        <Text fontSize={22} color='$white' fontWeight='500' ml='$2'>
-          {name}
-        </Text>
-      </Box>
-      <Text color='$textDark400' mt='$2'>
-        {statusCode}
-      </Text>
-    </Box>
-  );
-};
-
-const HeadersCard = ({ iconSvg: IconSvg, name, headers }) => {
-  return (
-    <Box
-      flexDirection='column'
-      borderWidth={1}
-      borderColor='$borderDark700'
-      sx={{
-        _web: {
-          flex: 1,
-        },
-      }}
-      m='$2'
-      p='$4'
-      rounded='$md'
-    >
-      <Box alignItems='center' display='flex' flexDirection='row'>
-        {/* <Image source={iconSvg} alt="document" width={22} height={22} /> */}
-        <Text>
-          <IconSvg />
-        </Text>
-        <Text fontSize={22} color='$white' fontWeight='500' ml='$2'>
-          {name}
-        </Text>
-      </Box>
-      <Text color='$textDark400' mt='$2' fontSize={8}>
-        {Object.entries(headers).map(([key, value]) => {
-          return (
-            <Text color='$textDark400' fontSize={8} key={key}>
-              {`${key}: ${value} \n`}
-            </Text>
-          );
-        })}
-      </Text>
-    </Box>
-  );
-};
-
-const Container = () => {
-  const [url, setUrl] = useState('');
   const [responseStatus, setResponseStatus] = useState(0);
   const [responseHeaders, setResponseHeaders] = useState({});
   const [response, setResponse] = useState('');
@@ -116,6 +54,7 @@ const Container = () => {
       try {
         const response = await fetch(url);
         for (const [key, value] of response.headers) {
+          console.log(key, value);
           setResponseHeaders((prev) => ({ ...prev, [key]: value }));
         }
         setResponseStatus(response.status);
@@ -135,131 +74,156 @@ const Container = () => {
     fetchData();
   };
 
+  const FirstRoute = () => {
+    return (
+      <Box>
+        <Text>First Route</Text>
+      </Box>
+    );
+  };
+
+  const SecondRoute = () => {
+    return (
+      <Box h={layout.height} bgColor='$warmGray800'>
+        <Text
+          selectable={true}
+          fontSize={12}
+          color='$green500'
+          style={{ fontFamily: 'monospace' }}
+        >
+          {response}
+        </Text>
+      </Box>
+    );
+  };
+
+  const ThirdRoute = () => {
+    return (
+      <Box h={layout.height} bgColor='$warmGray800'>
+        <Text
+          selectable={true}
+          fontSize={12}
+          color='$green500'
+          style={{ fontFamily: 'monospace' }}
+        >
+          {Object.entries(responseHeaders).map(([key, value]) => {
+            return (
+              <Text color='$textDark400' fontSize={8} key={key}>
+                {`${key}: ${value} \n`}
+              </Text>
+            );
+          })}
+        </Text>
+      </Box>
+    );
+  };
+
   return (
-    <Box flex={1} backgroundColor='$black'>
-      <ScrollView
-        style={{ height: '100%' }}
-        contentContainerStyle={{ flexGrow: 1 }}
+    <>
+      <Box style={{ paddingTop: insets.top }} />
+      <Box
+        px='$6'
+        paddingBottom='$2'
+        alignItems='center'
+        sx={{
+          '@base': {
+            flexDirection: 'row',
+          },
+          '@sm': {
+            flexDirection: 'row',
+          },
+          '@md': { alignSelf: 'flex-start' },
+        }}
       >
-        <Box
-          position='absolute'
-          sx={{
-            '@base': {
-              h: 500,
-              w: 500,
-            },
-            '@lg': {
-              h: 700,
-              w: 700,
-            },
-          }}
+        <Textarea
+          size='sm'
+          isReadOnly={false}
+          isInvalid={false}
+          isDisabled={false}
+          w='80%'
+          h='$10'
+          marginRight='$2'
         >
-          <Gradient />
-        </Box>
-        <Box
-          height='60%'
-          sx={{
-            '@base': {
-              my: '$16',
-              mx: '$5',
-              height: '80%',
-            },
-            '@lg': {
-              my: '$24',
-              mx: '$32',
-            },
-          }}
-          justifyContent='space-between'
-          alignItems='center'
+          <TextareaInput
+            value={url}
+            style={{
+              fontFamily: 'monospace',
+              fontSize: 11,
+              width: '100%',
+              overflow: 'hidden',
+            }}
+            onChangeText={(text) => setUrl(text.toLocaleLowerCase())}
+          />
+        </Textarea>
+        <Button
+          size='md'
+          variant='solid'
+          action='primary'
+          isDisabled={false}
+          isFocusVisible={false}
+          onPress={executeCall}
         >
-          <Box
-            bg='#64748B33'
-            px='$6'
-            alignItems='center'
-            sx={{
-              '@base': {
-                flexDirection: 'row',
-              },
-              '@sm': {
-                flexDirection: 'row',
-              },
-              '@md': { alignSelf: 'flex-start' },
-            }}
-          >
-            <Textarea
-              size='sm'
-              isReadOnly={false}
-              isInvalid={false}
-              isDisabled={false}
-              w='90%'
-              h='$10'
-              marginRight='$2'
-            >
-              <TextareaInput
-                color='$white'
-                placeholder='Your url...'
-                onChangeText={(text) => setUrl(text.toLocaleLowerCase())}
-              />
-            </Textarea>
-            <Button
-              size='md'
-              variant='solid'
-              action='primary'
-              isDisabled={false}
-              isFocusVisible={false}
-              onPress={executeCall}
-            >
-              <ButtonText>Execute</ButtonText>
-            </Button>
-          </Box>
-          <Box
-            sx={{
-              '@base': {
-                flexDirection: 'column',
-              },
-              '@md': {
-                flexDirection: 'column',
-              },
-            }}
-          >
-            <FeatureCard
-              iconSvg={Rocket}
-              name='Status'
-              statusCode={responseStatus}
-            />
-            <HeadersCard
-              iconSvg={DocumentData}
-              name='Headers'
-              headers={responseHeaders}
-            />
-          </Box>
-          <Box
-            sx={{
-              '@base': {
-                flexDirection: 'row',
-              },
-              '@md': {
-                flexDirection: 'column',
-              },
-            }}
-          >
-            <Textarea
-              isReadOnly={false}
-              isInvalid={false}
-              isDisabled={false}
-              w='100%'
-              h='$64'
-            >
-              <TextareaInput
-                fontSize={10}
-                style={{ fontFamily: 'monospace' }}
-                color='$white'
-                value={response}
-              />
-            </Textarea>
-          </Box>
-        </Box>
-      </ScrollView>
-    </Box>
+          <ButtonText>&gt;</ButtonText>
+        </Button>
+      </Box>
+      <Tab.Navigator initialRouteName='Response'>
+        <Tab.Screen name='Home' component={FirstRoute} />
+        <Tab.Screen
+          name='Response'
+          component={SecondRoute}
+          options={{
+            tabBarBadge: () => (
+              <Box
+                py='$1'
+                sx={{
+                  '@base': {
+                    flexDirection: 'row',
+                  },
+                  '@sm': {
+                    flexDirection: 'row',
+                  },
+                  '@md': { alignSelf: 'flex-start' },
+                }}
+              >
+                <Text
+                  size='2xs'
+                  px='$2'
+                  marginRight='$1'
+                  bg='$green400'
+                  rounded='$md'
+                >
+                  3
+                </Text>
+              </Box>
+            ),
+          }}
+        />
+        <Tab.Screen name='Headers' component={ThirdRoute} />
+      </Tab.Navigator>
+    </>
   );
-};
+}
+
+const Stack = createNativeStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <GluestackUIProvider config={config}>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='Root'
+              options={{
+                headerShown: false,
+              }}
+            >
+              {() => <Demo />}
+            </Stack.Screen>
+          </Stack.Navigator>
+        </GluestackUIProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
